f1d356dfd98d0f419c6fedcda4f69610d806d832
akm98 <akashmishra.akm@gmail.com>
2025-07-05 18:45:15 +0530

    feat: implemented get all api

--- src/controller/workoutController.ts
+++ src/controller/workoutController.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from 'express'
-import { WorkoutModel, zodWorkoutSchema } from '../model/workout.model';
+import { WorkoutModel, zodWorkoutSchema } from '../model/workout.model'
 
-
-const handleError = (res:Response, status: number , message:unknown) => {
-  let error = 'An unexpected error occurred';
-  if(typeof message === 'string') {
-    error = message;
+const handleError = (res: Response, status: number, message: unknown) => {
+  let error = 'An unexpected error occurred'
+  if (typeof message === 'string') {
+    error = message
   }
-	return res.status(status).json({
-		success: false,
-		error,
-	});
-};
+  return res.status(status).json({
+    success: false,
+    error
+  })
+}
 
-export const getAllWorkouts = (_: Request, res: Response) => {
+export const getAllWorkouts = async (_: Request, res: Response) => {
   try {
-    // Fetch all exercises from the database
-    // const exercises = await Exercise.find();
-    res.status(200).json({ message: 'Exercises fetched successfully' })
+    const workouts = await WorkoutModel.find().sort({ createdAt: -1 })
+
+    res.status(200).json({ data: workouts })
   } catch (error) {
     res.status(500).json({ message: 'Error fetching exercises', error })
   }
@@ -28,15 +27,15 @@ export const createWorkout = async (req: Request, res: Response) => {
     // Create a new workout in the database
     // const workout = await Workout.create(req.body);
 
-    if(zodWorkoutSchema.safeParse(req.body).success === false) {
-       handleError(res, 400, 'Invalid workout data');
-       return;
+    if (zodWorkoutSchema.safeParse(req.body).success === false) {
+      handleError(res, 400, 'Invalid workout data')
+      return
     }
-    await WorkoutModel.create(req.body);
+    await WorkoutModel.create(req.body)
 
     res.status(201).json({ message: 'Workout created successfully' })
   } catch (error: unknown) {
-   handleError(res, 500, error);
+    handleError(res, 500, error)
   }
-
 }
+
